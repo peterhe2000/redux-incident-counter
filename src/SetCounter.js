3c49d39c8be7d7a2aa91b43cbf7bef803831f0e9
Peter He <peter@mathspathway.com>
2021-12-26 09:42:31 +1100

    bind set action to the form

--- src/SetCounter.js
+++ src/SetCounter.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { set } from './actions';
+
 export const SetCounter = () => {
+  const [count, setCount] = useState(0);
+  const dispatch = useDispatch();
+
   return (
     <section className="controls">
       <form
         onSubmit={(event) => {
           event.preventDefault();
+          dispatch(set(count));
         }}
       >
         <label htmlFor="set-to">Set Count</label>
-        <input id="set-to" type="number" />
+        <input
+          id="set-to"
+          type="number"
+          value={count}
+          onChange={(event) => setCount(event.target.value)}
+        />
         <input type="submit" />
       </form>
     </section>
