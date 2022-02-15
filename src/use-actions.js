e4b86b0c43212306c2b7ed8e3837ae6b7ef716c2
Peter He <peter@mathspathway.com>
2022-02-15 23:58:07 +1100

    Added comments

--- src/use-actions.js
+++ src/use-actions.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 export const useActions = (actions) => {
+  // action is the same object, dispatch is the same object so this function is pure, could use memo
   const dispatch = useDispatch();
   return useMemo(
     () => bindActionCreators(actions, dispatch),
